b7bc5d3e32da8aefaa9425b6698089bb8264b2a3
Vense Chang <vense1023@gmail.com>
2020-08-12 00:08:05 +0800

    Add edit for typo of url

--- js/options.js
+++ js/options.js
@@ -27,6 +27,7 @@
             }
 
             settings[sc].push(url)
+            settings[sc] = settings[sc].sort()
             this.save()
 
             this.urlTarget.value = ''
@@ -65,15 +66,7 @@
                 content += `        ${sc}`
                 content += '    </div>'
                 targets[sc].forEach(target => {
-                    content += '    <div class="flex my-4">'
-                    content += '        <div class="w-1/6"> </div>'
-                    content += '        <div class="w-4/6 break-all flex items-center text-sm">'
-                    content += `            ${target}`
-                    content += '        </div>'
-                    content += '        <div class="w-1/6 text-center flex">'
-                    // content += `            <img data-action="click->setting#edit" data-params="${sc}" src='../images/edit.png' style='height: 24px; width: 24px;' class='mx-1 cursor-pointer' />`
-                    content += `            <img data-action="click->setting#delete" data-shortcut=${sc} data-params="${target}" src='../images/trash.png' style='height: 24px; width: 24px;' class='mx-1 cursor-pointer' />`
-                    content += '        </div>'
+                    content += `    <div class="flex my-4" data-controller="url" data-shortcut="${sc}" data-url="${target}">`
                     content += '    </div>'
                 })
                 content += '</div>'
@@ -116,6 +109,17 @@
             chrome.storage.sync.set({"settings": settings}, function() {})
         }
 
+        update = (sc, originUrl, url) => {
+            let sameVariableCountUrl = settings[sc].find(u => this.matchUrl(u).length === this.matchUrl(url).length)
+            if ( originUrl === url || sameVariableCountUrl !== undefined && !confirm(`Are you sure to replace 「${sameVariableCountUrl}」 with 「${url}」?`) ) {
+                this.reload()
+                return
+            }
+            settings[sc] = settings[sc].filter(u => this.matchUrl(u).length !== this.matchUrl(url).length)
+            settings[sc] = settings[sc].filter(u => u !== originUrl).concat(url.trim()).sort()
+            this.save()
+        }
+
         delete = (el) => {
             const sc = el.target.dataset.shortcut
             const target = el.target.dataset.params
@@ -141,6 +145,63 @@
                     this.reload()
                 }
             })
+        }
+    })
+
+    app.register("url", class extends Stimulus.Controller {
+        static get targets() {
+            return ['url']
+        }
+
+        edit = () => {
+            this.isEditing = true
+            this.reload()
+        }
+
+        update = () => {
+            if(this.urlTarget.value.trim() === '') {
+                this.urlTarget.value = ''
+                this.urlTarget.classList.add('border-red-500')
+                setTimeout(function(){ alert("This field can't be empty!") }, 100)
+                return
+            }
+
+            this.isEditing = false
+            this.settingController.update(this.sc, this.url, this.urlTarget.value)
+        }
+
+        // Stimulus Lifecycle
+        initialize() {
+            this.isEditing = false
+            this.url = this.element.dataset.url
+            this.sc  = this.element.dataset.shortcut
+            this.reload()
+        }
+
+        reload = () => {
+            let content = ''
+            content += '<div class="w-1/6"> </div>'
+            content += '<div class="w-4/6 break-all flex items-center text-sm">'
+            if (this.isEditing) {
+                content += `    <textarea type="text" class="border rounded-sm w-full mx-1 px-2" data-target="url.url">${this.url}</textarea>`
+            } else {
+                content += `    <span class="px-3">${this.url}</span>`
+            }
+            content += '</div>'
+            content += '<div class="w-1/6 text-center flex">'
+            if (this.isEditing) {
+                content += `    <img data-action="click->url#update" src='../images/save.png' style='height: 24px; width: 24px;' class='mx-1 cursor-pointer' />`
+            } else {
+                content += `    <img data-action="click->url#edit" src='../images/edit.png' style='height: 24px; width: 24px;' class='mx-1 cursor-pointer' />`
+                content += `    <img data-action="click->setting#delete" data-shortcut=${this.sc} data-params="${this.url}" src='../images/trash.png' style='height: 24px; width: 24px;' class='mx-1 cursor-pointer' />`
+            }
+            content += '</div>'
+            this.element.innerHTML = content
+        }
+
+        get settingController() {
+            let el = this.element.closest('[data-controller="setting"]')
+            return this.application.getControllerForElementAndIdentifier(el, "setting")
         }
     })
   })()
